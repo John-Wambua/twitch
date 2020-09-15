8f580a9c73c2dde7c170e72dfa447ac882b5500e
John-Wambua <jwambua307@gmail.com>
2020-09-15 11:36:22 +0300

    showing single streams

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import {
     Router,
     Route,
+    Switch
 } from "react-router-dom"
 import StreamList from "./streams/StreamList";
 import StreamCreate from "./streams/StreamCreate";
@@ -17,11 +18,13 @@ const App =()=>{
 
             <Router history={history}>
                 <Header/>
+                <Switch>
                 <Route path={"/"} exact component={StreamList}/>
                 <Route path={"/streams/new"} exact component={StreamCreate}/>
                 <Route path={"/streams/edit/:id"} exact component={StreamEdit}/>
                 <Route path={"/streams/delete/:id"} exact component={StreamDelete}/>
                 <Route path={"/streams/:id"} exact component={StreamShow}/>
+                </Switch>
             </Router>
         </div>
     )
--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {connect} from "react-redux"
+import {fetchStream} from "../../actions";
 
-const StreamShow=()=>{
-    return(
-        <div>StreamShow</div>
-    )
+const StreamShow=props=>{
+    const {id} =props.match.params
+    useEffect(()=>{
+        props.fetchStream(id)
+    },[])
+    if (!props.stream) return <div>Loading...</div>
+        return (
+            <div>
+                <h2>{props.stream.title}</h2>
+                <p>{props.stream.description}</p>
+            </div>
+        )
 }
+const mapStateToProps=(state,ownProps) =>{
+    return {stream: state.streams[ownProps.match.params.id]}
+}
+const mapStateToDispatch = {fetchStream}
 
-export default StreamShow;
+export default connect(
+    mapStateToProps,
+    mapStateToDispatch
+)(StreamShow);
